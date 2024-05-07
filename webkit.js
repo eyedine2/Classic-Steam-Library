afb7aef21aed9e301846a6f8f0128761e089021b
Ivan <65731940+eyedine2@users.noreply.github.com>
2024-05-07 14:03:33 -0400

    Create webkit.js

---
+++ webkit.js
@@ -0,0 +1,7 @@
+if (document.URL.indexOf('/news') >= 0){
+
+const newPath = window.location.href.replace('https://store.steampowered.com/news/', 'https://store.steampowered.com/oldnews/');
+
+window.location.href = newPath;
+
+};
