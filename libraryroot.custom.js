fdedddc7b154d6235393246cefb8caeca8477697
Ivan <65731940+eyedine2@users.noreply.github.com>
2024-05-07 14:02:45 -0400

    Update libraryroot.custom.js

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,7 +1,6 @@
-const h = '.appdetailsheader_HeaderFeatures_2b6Wk.Panel'
-const i = 'sharedappdetailsheader_TopCapsule_2meE3'
-const n = '.appdetailsprimarylinkssection_Links_DgVQa.Panel'
-
+const h = '._2b6WkTnmJxMuX1biL7aS3C.Panel'
+const i = '_2meE3N6-Wkw3yQUVtQK-i8'
+const n = '.DgVQapkBmhAW6oPY5rPZo.Panel'
 new MutationObserver((mo) => {
 	mo.forEach(function(m) {
 		m.addedNodes.forEach(function() {
