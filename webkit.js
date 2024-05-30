83ac502a795721baeec30b169ffb2af5e0a879c9
Ivan <65731940+eyedine2@users.noreply.github.com>
2024-05-30 02:08:48 -0400

    Update webkit.js

--- webkit.js
+++ webkit.js
@@ -1,3 +1,5 @@
+// Old News redirect, by Xammand
+
 if (document.URL.indexOf('/news') >= 0){
 
 const newPath = window.location.href.replace('https://store.steampowered.com/news/', 'https://store.steampowered.com/oldnews/');
